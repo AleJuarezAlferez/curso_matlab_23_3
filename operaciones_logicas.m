09adffa6133e8bcbf0283b2768949169b13383f7
AleJuarezAlferez <132621349+AleJuarezAlferez@users.noreply.github.com>
2023-05-04 17:19:17 -0600

    Add files via upload

---
+++ operaciones_logicas.m
@@ -0,0 +1,22 @@
+clc
+clear all
+close all
+
+% Son las operaciones cuyo resultado es un valor verdadero o un valor falso
+% AND Es una operacion cuyo resultado es 1, si ambos valores que se
+% combinan son 1, de lo contrario son 0
+
+% OR Es una operacion donde el resultado es 1, si alguno de los dos valores
+% es 1, si los dos son 0 el resultado es 0
+
+% NOT Es invertir el valor de verdad
+
+% AND = &
+% OR = |
+% NOT = ~
+% Verdadero = 1, true
+% Falso = 0, false
+
+operacion_1 = 0 & false;
+operacion_2 = true | false;
+operacion_3 = ~0
